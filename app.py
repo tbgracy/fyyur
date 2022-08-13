e4b0bb9d8a6f822435cda32573b327b08e1b5642
Tsierenana Bôtramanagna Gracy <gtsierenana@gmail.com>
2022-08-13 19:31:51 +0300

    Implement "Add Show" feature

--- app.py
+++ app.py
@@ -501,15 +501,22 @@ def create_shows():
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
     # called to create new shows in the db, upon submitting new show listing form
-    # TODO: insert form data as a new Show record in the db, instead
+    # TODO : Constraints
     show = Show(
-
+        venue_id = request.form['venue_id'],
+        artist_id = request.form['artist_id'],
+        start_time = request.form['start_time'],
     )
-    # on successful db insert, flash success
-    flash('Show was successfully listed!')
-    # TODO: on unsuccessful db insert, flash an error instead.
-    # e.g., flash('An error occurred. Show could not be listed.')
-    # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+    try:
+        db.session.add(show)
+        db.session.commit()
+        flash('Show was successfully listed!')
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+        flash('An error occurred. Show could not be listed')
+    finally:
+        db.session.close()
     return render_template('pages/home.html')
 
 
